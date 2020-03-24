1d3d3bdeaeebbfc03a1031f6f348e362552cbc95
dany_milan <esteban239222@gmail.com>
2020-03-24 10:41:12 -0400

    textinputs with styles

--- React Native/FirstApp/FirstApp/App.js
+++ React Native/FirstApp/FirstApp/App.js
@@ -7,24 +7,36 @@
  */
 
 import React from 'react';
-import {
-  StyleSheet,
-  View,
-} from 'react-native';
+import {StyleSheet, View, TextInput} from 'react-native';
 
-import {
-  Colors,
-} from 'react-native/Libraries/NewAppScreen';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 import MyComponent from './src/test/MyComponent';
-import Logo from './src/login/logo'
+import Logo from './src/login/logo';
+import User from './src/img/username.png';
+import Pass from './src/img/pass.png';
+import Input from './src/login/input';
 
 const App: () => React$Node = () => {
+  const [value, onChangeText] = React.useState('Useless Placeholder');
   return (
     <>
-      <View  style={styles.body}>
-        <View  style={styles.container}>
-          <MyComponent style={styles.box} text = "Cosa a mostrar"></MyComponent>
-          <Logo/>
+      <View style={styles.body}>
+        <View style={styles.container}>
+          <MyComponent style={styles.box} text="Cosa a mostrar"></MyComponent>
+          <Logo />
+          <Input
+            source={User}
+            placeholder="Username"
+            secureTextEntry={false}
+            autoCorrect={false}
+          />
+           <Input
+            source={Pass}
+            placeholder="Password"
+            secureTextEntry={false}
+            autoCorrect={false}
+          />
+          
         </View>
       </View>
     </>
@@ -32,19 +44,20 @@ const App: () => React$Node = () => {
 };
 
 const styles = StyleSheet.create({
-  body:{
-    flex:1,
-    flexDirection:'column',
-    backgroundColor:Colors.primary,
-  },
-  container:{
-    flex:0.5,
+  body: {
+    flex: 1,
     flexDirection: 'column',
-    alignItems:'center'
+    backgroundColor: Colors.primary,
   },
-  box:{
-    height:100
-  }
+  container: {
+    flex: 0.5,
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  box: {
+    height: 100,
+  },
+
 });
 
 export default App;
