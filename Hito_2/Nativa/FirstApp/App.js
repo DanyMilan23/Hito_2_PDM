eb553012b5cf7bec157e87cb93eab7c5e1050935
dany_milan <esteban239222@gmail.com>
2020-04-11 01:38:31 -0400

    Hito 2 Ordenado

---
+++ Hito_2/Nativa/FirstApp/App.js
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+//npx react-native run-android
+import React from 'react';
+import LoginScreen from './src/View/login/loginScreen'
+
+
+
+const App: () => React$Node = () => {
+  
+  return (
+    <>
+        <LoginScreen/>
+      
+    </>
+  );
+};
+
+
+
+export default App;
